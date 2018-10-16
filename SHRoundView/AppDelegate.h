0c96e10af778a7b3a71337b44743bb2c92e97ef2
shihu <13261089216@163.com>
2018-10-16 11:51:45 +0800

    Initial Commit

---
+++ SHRoundView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHRoundView
+//
+//  Created by joyshow on 2018/10/16.
+//  Copyright © 2018年 石虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHRoundView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHRoundView
+//
+//  Created by joyshow on 2018/10/16.
+//  Copyright © 2018年 石虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
